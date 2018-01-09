086b156a3fa24c3c0c91f74fbe0abdbb8d6dbd80
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-09 14:18:23 +0000

    Integrated MeVC

---
+++ Breakpoint/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by Hayden Jamieson on 09/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emaillbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        
+    }
+}
