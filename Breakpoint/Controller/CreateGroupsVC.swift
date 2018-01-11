080c757f7dcedb6dc9162648efe770f826c81412
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-11 18:51:53 +0000

    Configured Table view for users in CreateGroupVC

--- Breakpoint/Controller/CreateGroupsVC.swift
+++ Breakpoint/Controller/CreateGroupsVC.swift
@@ -20,7 +20,8 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
 
@@ -29,6 +30,28 @@ class CreateGroupsVC: UIViewController {
     }
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
     }
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else {
+            return UITableViewCell()
+        }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "marty@b2f.com", isSelected: true)
+        return cell
+    }
+    
+    
 }
