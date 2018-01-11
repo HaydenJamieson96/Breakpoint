3c81b77ef77d0f7c1b794ea7f3b8547d91b71a7a
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-11 18:04:00 +0000

    CreateGroupsVC UI configured

---
+++ Breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,34 @@
+//
+//  CreateGroupsVC.swift
+//  Breakpoint
+//
+//  Created by Hayden Jamieson on 11/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTxtField: InsetTextField!
+    @IBOutlet weak var descriptionTxtField: InsetTextField!
+    @IBOutlet weak var emailSearchTxtField: InsetTextField!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        
+    }
+}
