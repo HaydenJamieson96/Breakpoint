f8314759fcadf9c01c328b7411edd0ea5d731fb3
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-12 15:09:10 +0000

    Group model class configured
    Table view for groups configured

--- Breakpoint/Controller/GroupsVC.swift
+++ Breakpoint/Controller/GroupsVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class GroupsVC: UIViewController {
 
     @IBOutlet weak var groupsTableView: UITableView!
+    var groupsArray = [Group]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,16 @@ class GroupsVC: UIViewController {
         groupsTableView.dataSource = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.groupsTableView.reloadData()
+            }
+        }
+    }
+    
 }
 
 extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
@@ -27,14 +38,15 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "GroupCell") as? GroupCell else {
             return UITableViewCell()
         }
-        cell.configureCell(title: "Nerd Herd", description: "Nerdiest", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        cell.configureCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         return cell
     }
     
